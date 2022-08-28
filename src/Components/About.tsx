d1e3f460c4b0e459231151b0df365a9688c69d22
galanca <cesar.galan@colorado.edu>
2022-08-28 17:07:12 -0600

    Add some write up to the page.

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -19,30 +19,21 @@ class About extends Component <any> {
 			<section id="about">
 				<div className="row">
 					<div className="three columns">
-							<img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+							<img className="profile-pic"  src={profilepic} />
 					</div>
 					<div className="nine columns main-col">
-							<h2>About Me</h2>
-
-							<p>{bio}</p>
-							<div className="row">
-								<div className="columns contact-details">
-										<h2>Contact Details</h2>
-										<p className="address">
+						<h2>About Me</h2>
+						<p>{bio}</p>
+					<div className="row">
+						<div className="columns contact-details">
+							<h2>Contact Details</h2>
+							<p className="address">
 								<span>{name}</span><br />
-								<span>{street}<br />
-											{city} {state}, {zip}
-										</span><br />
-								<span>{phone}</span><br />
-											<span>{email}</span>
+									{city}, {state}<br />
+								<span>{email}</span>
 							</p>
-								</div>
-								<div className="columns download">
-										<p>
-											<a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-										</p>
-								</div>
-							</div>
+						</div>
+					</div>
 					</div>
 				</div>
    		</section>
