c6c0250af82ca15f93b9048783bd65711f9eac6f
galanca <cesar.galan@colorado.edu>
2022-08-29 18:53:53 -0600

    Fix warnings.

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -7,19 +7,19 @@ class About extends Component <any> {
 			var name = this.props.data.name;
 			profilepic= "images/"+this.props.data.image;
 			var bio = this.props.data.bio;
-			var street = this.props.data.address.street;
+			// var street = this.props.data.address.street;
 			var city = this.props.data.address.city;
 			var state = this.props.data.address.state;
-			var zip = this.props.data.address.zip;
-			var phone= this.props.data.phone;
+			// var zip = this.props.data.address.zip;
+			// var phone= this.props.data.phone;
 			var email = this.props.data.email;
-			var resumeDownload = this.props.data.resumedownload;
+			// var resumeDownload = this.props.data.resumedownload;
 		}
 		return (
 			<section id="about">
 				<div className="row">
 					<div className="three columns">
-							<img className="profile-pic"  src={profilepic} />
+							<img className="profile-pic"  src={profilepic} alt="Picture of a profile"/>
 					</div>
 					<div className="nine columns main-col">
 						<h2>About Me</h2>
